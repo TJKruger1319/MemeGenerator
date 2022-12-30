e28317789376afd7fc41c1652ca7e86fb201534c
TJKruger1319 <119986326+TJKruger1319@users.noreply.github.com>
2022-12-29 20:35:00 -0500

    Add files via upload

---
+++ gen.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('#memeform');
+const results = document.querySelector('#results');
+
+form.addEventListener(
+    'submit', function(e) {
+    e.preventDefault();
+    results.innerHTML = '';
+    const image = document.querySelector('input[name="image"]:checked').id;
+    const textInput = document.querySelector('input[name="memetext"]').value;
+    newMeme = createMeme(image, textInput);
+    results.appendChild(newMeme);
+    form.reset();
+    }
+);
+
+function createMeme(image, text) {
+    const meme = document.createElement('div');
+    meme.classList.add("newDiv");
+    const img = document.createElement("img");
+    const p = document.createElement("p");
+    p.innerText = text;
+    meme.appendChild(p);
+    p.classList.add("memeText");
+    img.src = "/meme_generator/images/"+image+".jpg";
+    img.classList.add("newMeme");
+    meme.append(img);
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete!";
+    deleteButton.addEventListener('click', function(e){
+        results.innerHTML = '';  
+    }); 
+    deleteButton.classList.add("deleteButton");
+    meme.appendChild(deleteButton);
+        
+    
+    return meme;
+}
